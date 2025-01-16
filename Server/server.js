bc8d25cabf9c69408662210145fb2803e8997398
inijona <aj.pangondion@gmail.com>
2025-01-16 15:08:42 +0700

    initial commit

--- Server/server.js
+++ Server/server.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const { MongoClient, GridFSBucket, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const jwtSecret = 'rahasiawkwk';
 
 // Middleware
